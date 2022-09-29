343e9b901d82054f5f09049d4dcc18cf0ad85a01
aykae <abdrai19@gmail.com>
2022-09-29 14:50:49 -0400

    basic text

---
+++ basicText.py
@@ -0,0 +1,24 @@
+import board
+import terminalio
+import adafruit_matrixportal.matrixportal import MatrixPortal
+
+WIDTH = 64
+HEIGHT = 32
+FONT = "/IBMPlexMono-Medium-24_jep.bdf"
+
+mp = MatrixPortal(width=WIDTH, height=HEIGHT)
+
+mp.add_text(
+    text_font = FONT,
+    text_position = (
+        0,0
+        #(mp.graphics.display.width)
+    ),
+    text_color=0x800000
+)
+
+mp.set_text("LUMBA")
+
+while True:
+    continue
+    #time.sleep()
