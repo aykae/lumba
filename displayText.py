78528d493946c89f8980ed0c024c5282cb6bb129
aykae <raiaboudi@yahoo.com>
2022-09-30 21:14:04 -0400

    basic text

--- displayText.py
+++ displayText.py
@@ -1,4 +1,6 @@
 import displayio
+import terminalio
+from adafruit_display_text import label
 from adafruit_matrixportal.matrix import Matrix
 
 matrix = Matrix(width=64, height=32)
@@ -10,14 +12,26 @@ palette = displayio.Palette(2)
 palette[0] = 0x000000
 palette[1] = 0xffffff
 
+for i in range(display.width):
+    bitmap[i, 0] = 1
+
+#Display bitmap using tile grids and groups
+
 tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
 group = displayio.Group()
-group.append(tile_grid)
+#group.append(tile_grid)
 
 display.show(group)
 
-for i in range(display.width):
-    bitmap[i, 0] = 1
+#Display text using labels
+
+text = "LUMBA"
+text_area = label.Label(terminalio.FONT, text=text, background_tight=True, background_color=0x800000)
+text_area.x = 0
+text_area.y = 4#text_area.bounding_box[3] // 2
+print(text_area.bounding_box)
+group.append(text_area)
+#display.show(text_area)
 
 while True:
     pass
