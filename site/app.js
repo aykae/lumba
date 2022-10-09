c0bcde341c1d1ca32fe9921c1e10b10884adf8d6
aykae <raiaboudi@yahoo.com>
2022-10-08 21:15:22 -0400

    working on app.js for site

--- site/app.js
+++ site/app.js
@@ -1,3 +1,16 @@
 function onButtonClick() {
     console.log("button clicked");
 }
+
+function onButtonClick() {
+    //let service
+
+    navigator.bluetooth.requestDevice({filters: [{services: []}]})
+        .then(device => {
+            return device.gatt.connect()
+        })
+        .then(server => {
+            return server.getPrimaryService(serviceUuid);
+        })
+
+}
